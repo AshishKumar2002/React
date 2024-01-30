4d44c7b1c1cf56f4e34c6d013b8f0ddd89cc8163
AshishKumar2002 <ashishppt01@gmail.com>
2024-01-30 08:29:26 +0530

    initail commit

---
+++ README.md
@@ -0,0 +1,14 @@
+#parcel
+-Dev Build
+-Local Server
+-HMR=Hot Module Replacement
+-File watching Algorithm
+-Caching-Faster Build
+-Image Optimization
+-Bundling 
+-Compress
+-Consistent Hashing
+-Differential Bundlinhg - to support older browser
+-Code spliting
+-Error Handling
+-Tree shaking - remove unused item
--- app.js
+++ app.js
@@ -1,18 +1,12 @@
-/**
- * <div id="parent">
- *       <div id="child">
- *              <h1>Hello React</h1>
- *              <h2>Hello React</h2>
- *       </div>
- *  </div>
- * ReactElement(obejct)=>HTML(Browser understand)
- */
-
+import React from "react";
+import ReactDOM  from "react-dom/client";
 const parent = React.createElement(
   "div",
   { id: "parent" },
-    React.createElement("div", { id: "child" }, [
+  React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
     React.createElement("h2", {}, "I am h2 tag"),
   ])
 );

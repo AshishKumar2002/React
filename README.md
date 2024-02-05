5eae4f4541cb48b1e0234752c15c643184e90494
AshishKumar2002 <ashishppt01@gmail.com>
2024-02-05 22:35:03 +0530

    new changes

--- README.md
+++ README.md
@@ -5,10 +5,33 @@
 -File watching Algorithm
 -Caching-Faster Build
 -Image Optimization
--Bundling 
+-Bundling
 -Compress
 -Consistent Hashing
 -Differential Bundlinhg - to support older browser
 -Code spliting
 -Error Handling
 -Tree shaking - remove unused item
+//JSX(transpiled before it reaches the JS engine)-Parcel-Babelp
+//JSX => react Element => react /js object --> HTML(render)
+
+/react element
+// const jsxheading = (
+// <h1 className="heading" tabIndex="1">
+// Hello React
+// </h1>
+// );
+//React element=>React Object ==> when we render this to dom the it become html element
+//const heading = React.createElement("h1",{id:"heading"},"This is a react element");
+//React Functional component
+const TitleComponent = () => (
+
+  <h1 className="title" tabIndex="2">Hello New component</h1>
+);
+const HeadingComponent = () =>(
+  <div id="container">
+  <TitleComponent/>
+  <h2>{100+290}</h2>
+    <h1 className="heading">Namaste React Functional component</h1>
+  </div>
+);
--- app.js
+++ app.js
@@ -1,23 +1,70 @@
 import React from "react";
-import ReactDOM  from "react-dom/client";
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ])
-);
+import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "hello world from react"
-// );
-
-console.log(parent); //return object
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+          className="logo"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/iivuhjc2mswi9lublktf"
+      />
+      <h3>Meghana Foods</h3>
+      <h4>Briyani, North Indian, Asian</h4>
+      <h4>4.5 stars</h4>
+      <h4>30 mins</h4>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<AppLayout />);
